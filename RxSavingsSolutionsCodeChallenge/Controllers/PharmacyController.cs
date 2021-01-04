4b63b6faaefbe7a38ad3c0c32a74c67db800ec64
Brian Burns <bdburns6389@gmail.com>
2021-01-04 16:58:01 -0600

    Add project files.

---
+++ RxSavingsSolutionsCodeChallenge/Controllers/PharmacyController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RxSavingsSolutionsCodeChallenge.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PharmacyController : ControllerBase
+    {
+        private readonly ILogger<PharmacyController> _logger;
+
+        public PharmacyController(ILogger<PharmacyController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello World";
+        }
+    }
+}

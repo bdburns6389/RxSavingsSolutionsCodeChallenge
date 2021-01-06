ff351328b06a19cf573079d31f38d0d56e541821
brian <brian@DESKTOP-OI5E5JK>
2021-01-06 16:06:22 -0600

    Code cleanup.

--- RxSavingsSolutionsCodeChallenge/Controllers/PharmacyController.cs
+++ RxSavingsSolutionsCodeChallenge/Controllers/PharmacyController.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using RxSavingsSolutionsCodeChallenge.Domain;
-using RxSavingsSolutionsCodeChallenge.Domain.Models;
 using RxSavingsSolutionsCodeChallenge.Dtos;
 using RxSavingsSolutionsCodeChallenge.Helpers;
 
--- RxSavingsSolutionsCodeChallenge/Domain/Models/Pharmacy.cs
+++ RxSavingsSolutionsCodeChallenge/Domain/Models/Pharmacy.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace RxSavingsSolutionsCodeChallenge.Domain.Models
+﻿namespace RxSavingsSolutionsCodeChallenge.Domain.Models
 {
     public class Pharmacy
     {
